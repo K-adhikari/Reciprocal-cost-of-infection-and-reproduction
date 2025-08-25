d719c6f7245804c099b53521cbfbb6922d5af277
K-adhikari <152343178+K-adhikari@users.noreply.github.com>
2025-08-25 10:47:11 -0400

    Update Bacterial_load.R

--- Rscripts/Bacterial_load.R
+++ Rscripts/Bacterial_load.R
@@ -9,9 +9,13 @@ setwd()
 Data<- read.csv("Bacterial_load.csv", header=T)
 
 # Figuring out the highest number of CFU to replace the CFU value in rows with flag "T"
+
 max(Data$Count.per.Frame, na.rm = TRUE)
 
+
+
 # Assigning the numbers for rows with flag "T" i.e. too many colonies to count
+
 Updated_data<- Data %>% mutate(Count.per.Frame = case_when(Flags %in% c('T') ~ '485',
                                                                        TRUE ~ as.character(Count.per.Frame)),
                                            Count.per.ml = case_when(Flags %in% c('T') ~ '225000',
@@ -27,7 +31,9 @@ Updated_data$Count_per_fly<- as.numeric(Updated_data$Count_per_fly)
 Updated_data$ln_count<- as.numeric(Updated_data$ln_count)
 
 
-#Plotting the data
+
+# Plotting the data
+
 Updated_data$Treatment<- factor(Updated_data$Treatment, levels=c("Control", "Unmated","Mated"))
 Updated_data$Time<- as.factor(Updated_data$Time)
 
@@ -38,7 +44,9 @@ ggplot(Updated_data, aes(Time, ln_count, color=Treatment)) + geom_boxplot(width=
                                                                           show.legend = FALSE)
 
 
-#Mixed effect models to compare effect of experimental block
+
+# Mixed effect models to compare effect of experimental block
+
 New_data<- Updated_data[!Updated_data$Treatment=="Control",]
 New_data$Block<- as.factor(New_data$Block)
 
@@ -48,18 +56,26 @@ anova(Model1, Model2)
 summary(Model1)
 
 
-#Levene's test to compare if the variances are significantly different from each other or not
+
+# Levene's test to compare if the variances are significantly different from each other or not
+
 leveneTest(ln_count ~ Treatment, New_data)
 
-#Comparing variance between mated and unmated treatments
+
+
+# Comparing variance between mated and unmated treatments
+
 New_data %>% group_by(Treatment) %>% summarise(var=var(ln_count))
 
 
 
-#Welch's t-est the difference in mean between two treatment groups as they have signifincant difference in their variance
+# Welch's t-est the difference in mean between two treatment groups as they have signifincant difference in their variance
+
 t.test(ln_count~Treatment, New_data)
 
-###Fisher's exact test on the number of dead individuals between mated and unmated groups. First numbers in matrix represent dead and second number represent alive individuals
+
+
+# Fisher's exact test on the number of dead individuals between mated and unmated groups. First numbers in matrix represent dead and second number represent alive individuals
 A<- matrix(c(7,208, 107,247), nrow = 2)
 B<- fisher.test(A)
 B
